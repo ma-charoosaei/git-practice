07e8f0a7e61d2b9b320bf863329f89fce4e67a13
ma-charoosaei <m.a.charoosaei@gmail.com>
2023-04-16 04:19:52 +0430

    adding main func

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import utils
 if __name__ == '__main__':
     print (utils.add(3,4))
     
+def main():
+    print(utils.add(3,4))
+    print("Done add 3+4")
+    
+if __name__ == '__main__':
+    main()
