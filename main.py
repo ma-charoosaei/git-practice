134d804fce4619793ed1ba8d5426981881449fef
ma-charoosaei <m.a.charoosaei@gmail.com>
2023-04-17 20:15:57 +0430

    update in main branch

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import numpy as np
 import utils
 
-if __name__ == '__main__':
-    print (utils.add(3,4))
-    
-print("The main script was runned successfully")
-def main():
-    print(utils.add(3,4))
-    print("Done add 3+4")
-    
-if __name__ == '__main__':
-    main()
+print('ali charoos')
