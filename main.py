dcad90b62fd3b17d789e58ad4d79a9ddb24fdc9b
ma-charoosaei <m.a.charoosaei@gmail.com>
2023-04-17 19:56:54 +0430

    printing my name in main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import numpy as np
 import utils
 
-if __name__ == '__main__':
-    print (utils.add(3,4))
-    
-def testing():
-    return 'test'
-    
-assert testing() == 'test'
-
-print('Done ...')
+print('M Charoosaei')
